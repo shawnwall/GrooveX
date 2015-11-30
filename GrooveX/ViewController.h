fa67f7f2109f30e099a87f503043108bcabca2f8
Shawn Wall <shawnkwall@gmail.com>
2015-11-30 10:37:13 -0500

    Initial commit

--- GrooveX/ViewController.h
+++ GrooveX/ViewController.h
@@ -7,9 +7,11 @@
 //
 
 #import <Cocoa/Cocoa.h>
+#import <WebKit/WebKit.h>
 
 @interface ViewController : NSViewController
 
+@property (weak) IBOutlet WebView *webView;
 
 @end
 
--- GrooveX/ViewController.m
+++ GrooveX/ViewController.m
@@ -14,6 +14,12 @@
     [super viewDidLoad];
 
     // Do any additional setup after loading the view.
+    NSURL *url = [NSURL URLWithString:@"https://music.microsoft.com"];
+    NSURLRequest *urlRequest = [NSURLRequest requestWithURL:url];
+    self.webView.customUserAgent = @"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.75.14 (KHTML, like Gecko) Version/7.0.3 Safari/7046A194A";
+    [self.webView.preferences setPlugInsEnabled:true];
+    [self.webView setLayerUsesCoreImageFilters:YES];
+    [[[self webView] mainFrame] loadRequest:urlRequest];
 }
 
 - (void)setRepresentedObject:(id)representedObject {
